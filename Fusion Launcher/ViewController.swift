e1840119af50bb5ebf0f269a64083c6fbca931cf
Jinhyung Lim <jinhyung2003@naver.com>
2020-07-27 23:36:51 +0800

    Initial Commit

---
+++ Fusion Launcher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Fusion Launcher
+//
+//  Created by Jinhyung Lim on 2020/07/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
